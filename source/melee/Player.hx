887a8fe2fae2248131f53783b713784ef48ff822
Oscar Cederberg <os7138ce-s@student.lu.se>
2022-11-12 19:59:00 +0100

    Add movement tweening

--- source/melee/Player.hx
+++ source/melee/Player.hx
@@ -1,5 +1,7 @@
 package melee;
 
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
 import flixel.math.FlxPoint;
 import flixel.FlxSprite;
 
@@ -13,14 +15,16 @@ class Player extends FlxSprite
 {
     public static inline final SPEED_WALK:Int = 24;
 
-    var state:PlayerState;
+    var currentState:PlayerState;
+    var prevState:PlayerState;
     var moveSpeed:Int;
 
     public function new(x:Float, y:Float)
     {
         super(x, y);
 
-        this.state = Idle;
+        this.currentState = Idle;
+        this.prevState = Idle;
         this.facing = LEFT;
         this.moveSpeed = SPEED_WALK;
 
@@ -28,11 +32,13 @@ class Player extends FlxSprite
         setFacingFlip(LEFT, false, false);
 		setFacingFlip(RIGHT, true, false);
         setGraphicSize(2 * 16, 2 * 16);
+        updateHitbox();
     }
 
     override public function update(elapsed:Float):Void
 	{
 		handleInput();
+        animate();
 
 		super.update(elapsed);
 	}
@@ -61,9 +67,39 @@ class Player extends FlxSprite
         dy = _down ? 1 : dy;
 
         this.velocity = this.moveSpeed * new FlxPoint(dx, dy).normalize();
-
-        this.state = (dx != 0 || dy != 0) ? Walking : Idle;
         this.facing = (dx == -1) ? LEFT : this.facing;
         this.facing = (dx == 1) ? RIGHT : this.facing;
+        this.prevState = currentState;
+        this.currentState = (dx != 0 || dy != 0) ? Walking : Idle;
+    }
+
+    public function animate()
+    {
+        if (this.prevState == Idle)
+        {
+            if (this.currentState == Walking)
+            {
+                var from = this.facing == LEFT ? -20 : 20;
+                var to = -from;
+                var duration = Math.abs(this.angle) == 0 ? 0.25 : 0.25 * Math.abs(this.angle) / 20;
+                FlxTween.cancelTweensOf(this);
+                FlxTween.angle(this, this.angle, to, duration, {
+                    ease: FlxEase.bounceInOut, onComplete: tween -> {
+                            FlxTween.angle(this, to, from, 0.5, {
+                                type: FlxTweenType.PINGPONG, ease: FlxEase.bounceInOut
+                            });
+                        }
+                });
+            }
+        } else if (this.prevState == Walking) {
+            if (this.currentState == Idle)
+            {
+                var duration = Math.abs(this.angle) == 0 ? 0.25 : 0.25 * Math.abs(this.angle) / 20;
+                FlxTween.cancelTweensOf(this);
+                FlxTween.angle(this, this.angle, 0, duration, {
+                    ease: FlxEase.bounceInOut
+                });
+            }
+        }
     }
 }
