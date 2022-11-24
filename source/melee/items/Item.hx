36eb4d969b7e6a0e1020ec03f095395559139293
Oscar Cederberg <os7138ce-s@student.lu.se>
2022-11-24 21:51:05 +0100

    Add bounce to items

--- source/melee/items/Item.hx
+++ source/melee/items/Item.hx
@@ -1,5 +1,7 @@
 package melee.items;
 
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
 import flixel.FlxSprite;
 
 abstract class Item extends FlxSprite
@@ -7,6 +9,10 @@ abstract class Item extends FlxSprite
     public function new(x:Float, y:Float)
     {
         super(x, y);
+
+        FlxTween.tween(this, {"offset.y": offset.y - 8}, 1, {
+            type: FlxTweenType.PINGPONG, ease: FlxEase.sineInOut
+        });
     }
 
     public abstract function interact(player:Player):Void;
